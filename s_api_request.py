77deefa7daada4708e26663e7629dc99d0f8750c
Ratana Khek <ratanakhek22@gmail.com>
2025-11-06 21:33:07 -0600

    added comments and revised error handles

--- s_api_request.py
+++ s_api_request.py
@@ -21,6 +21,8 @@ def set_user():
     
     # make requests to LoL API 
     puuid = lolapi_puuid(sname=request.cookies.get("sname"), tag=request.cookies.get("tag"))
+    if None == puuid:
+        return make_response(jsonify({"error": "summoner not found"}))
 
     response = make_response(jsonify({"message": True}))
     response.set_cookie("sname", name, max_age=60*60*24)
@@ -101,16 +103,41 @@ def ai_coach():
 
 # LoL API Requests
 def lolapi_puuid(sname: str, tag: str) -> str:
+    """_summary_
+
+    Args:
+        sname (str): summoner name
+        tag (str): tagline of summoner
+
+    Returns:
+        str: corresponding summoner's PUUID
+    """
     # Player Info
     api_request = f"https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{sname}/{tag}&api_key={APIKEY_LOL}"
     resp = requests.get(api_request)
+    
+    if resp.status_code != 200:
+        return None
     
     return resp.json()["puuid"]
 
 
 def lolapi_matches(puuid: str) -> dict:
+    """_summary_
+
+    Args:
+        puuid (str): player's PUUID
+
+    Returns:
+        dict: player -> player's match data, opponent -> lane opponent's match data
+        None: errors return None
+    """
     api_url_matches = f"https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count=20&api_key={APIKEY_LOL}"
     resp = requests.get(api_url_matches)
+    
+    if resp.status_code != 200:
+        return None
+    
     matchdata = resp.json()
     
     # Champions for the specific match
@@ -127,6 +154,7 @@ def lolapi_matches(puuid: str) -> dict:
         "opponent": opponent,
     }
 
+# other GET functions
 def getchampdata(championname, folderpath="champion"):
     filename = f"{championname}.json"
     filepath = os.path.join(folderpath, filename)
@@ -136,6 +164,8 @@ def getchampdata(championname, folderpath="champion"):
 
     return data
 
+def get_playerData():
+    return
 # playerData_json = None # Check for None in case file load fails
 # with open("./playerData/playerData.json", "r") as file:
 #     playerData = json.load(file)
