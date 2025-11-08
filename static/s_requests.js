8a5f4e511fe6d56ac2005f6387e1c9c1131521b4
Ratana Khek <ratanakhek22@gmail.com>
2025-11-07 20:14:09 -0600

    restructure lmao

---
+++ static/s_requests.js
@@ -0,0 +1,25 @@
+window.onload = () => {
+    const test_btn = document.getElementById("test");
+
+    test_btn.addEventListener("click", async () => {
+    try {
+      // Send the fetch request to Flask server to set up user
+      const response = await fetch("http://127.0.0.1:5000/aws/ai_traits", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        credentials: "include",
+        body: JSON.stringify({})
+      });
+
+      // Parse the response from Flask
+      const data = await response.json();
+
+      console.log("Server Response:", data);
+
+    } catch (error) {
+      console.error("Error during fetch:", error);
+    }
+    });
+};
