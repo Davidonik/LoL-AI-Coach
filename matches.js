c3b1c4846d32f793a2900012ba415834abe5d964
David Pham <davidphamlong0@gmail.com>
2025-11-07 19:24:26 -0600

    matches

--- matches.js
+++ matches.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   const loading = document.getElementById("loading");
 
   try {
-    const response = await fetch("/get_matches");
+    const response = await fetch("/get_matche");
     const data = await response.json();
 
     if (response.ok && data.matches) {
