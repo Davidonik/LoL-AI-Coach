809d65c409a8dbad5e9621b20b1b01ffbb7b7bd7
Ratana Khek <ratanakhek22@gmail.com>
2025-11-09 00:24:48 -0600

    nav fixed

--- static/s_review.js
+++ static/s_review.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
- const home = document.getElementById('home');
- const leaderboard = document.getElementById('leaderboard');
+  const home = document.getElementById('home');
+  const leaderboard = document.getElementById('leaderboard');
 
   if (home){
     home.addEventListener("click", () => {
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
   if (leaderboard){
-    home.addEventListener("click", () => {
+    leaderboard.addEventListener("click", () => {
       window.location.href = "http://127.0.0.1:5000/leaderboard";
     });
   }
