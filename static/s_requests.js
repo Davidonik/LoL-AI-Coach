c509fc5e4c890860576a5ee81411f35907b463f9
Ratana Khek <ratanakhek22@gmail.com>
2025-11-08 20:33:14 -0600

    ai js

--- static/s_requests.js
+++ static/s_requests.js
@@ -1,24 +1,48 @@
 window.onload = () => {
-    const test_btn = document.getElementById("test");
+  const review_btn = document.getElementsByClassName("review");
+  review_btn.addEventListener("click", async () => {
+    try {
+      // Send the fetch request to Flask server to let ai coach review
+      const response = await fetch("http://127.0.0.1:5000/aws/ai_coach", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        credentials: "include",
+          body: JSON.stringify({
+          "matchid": "HOLD" // holder string should get from button data
+        })
+      });
 
-    test_btn.addEventListener("click", async () => {
-      try {
-        // Send the fetch request to Flask server to set up user
-        const response = await fetch("http://127.0.0.1:5000/aws/ai_traits", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          credentials: "include"
-        });
+      // Parse the response from Flask
+      const data = await response.json();
 
-        // Parse the response from Flask
-        const data = await response.json();
+      console.log("Server Response:", data);
 
-        console.log("Server Response:", data);
+    } catch (error) {
+      console.error("Error during fetch:", error);
+    }
+  });
 
-      } catch (error) {
-        console.error("Error during fetch:", error);
-      }
-    });
+  const test_btn = document.getElementById("test");
+  test_btn.addEventListener("click", async () => {
+    try {
+      // Send the fetch request to Flask server to get traits
+      const response = await fetch("http://127.0.0.1:5000/aws/ai_traits", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        credentials: "include"
+      });
+
+      // Parse the response from Flask
+      const data = await response.json();
+
+      console.log("Server Response:", data);
+
+    } catch (error) {
+      console.error("Error during fetch:", error);
+    }
+  });
 };
