774209418a18edac2336af5a856441054ebf50b3
Figo Fontana <Figo>
2025-11-10 12:52:57 -0600

    m

--- static/s_load_leaderboard.js
+++ static/s_load_leaderboard.js
@@ -16,7 +16,51 @@ document.addEventListener("DOMContentLoaded", async () => {
         if (data) {
             for (let i = 0; i < Math.min(10, data.board.length); i++) {
                 leaderboard_display.innerHTML += `
-                    <code for each row of stats>
+                    <div class="llb-row">
+                      <!-- Summoner -->
+                      <div class="llb-cell">
+                        <div class="llb-sum">
+                          <div class="llb-name">
+                            ${data.board[i].ign_.gameName} <span class="llb-tag">${data.board[i].ign_.tagLine}</span>
+                          </div>
+                        </div>
+                      </div>
+                          
+                      <!-- KDA -->
+                      <div class="llb-cell">
+                        <span class="llb-kda">${(data.board[i].KDA_.kills + data.board[i].KDA_.assists)/Math.max(data.board[i].KDA_.deaths, 1)}</span>
+                        <span class="llb-kda-detail">${data.board[i].KDA_.kills} / ${data.board[i].KDA_.deaths} / ${data.board[i].KDA_.assists}</span>
+                      </div>
+                          
+                      <!-- Damage -->
+                      <div class="llb-cell llb-right">
+                        <span class="llb-chip">${data.board[i].total_.totalDamageDealtToChampions}</span>
+                      </div>
+                          
+                      <!-- Gold -->
+                      <div class="llb-cell llb-right">
+                        <span class="llb-chip">${data.board[i].total_.goldEarned}</span>
+                      </div>
+                          
+                      <!-- Turrets -->
+                      <div class="llb-cell llb-right">
+                        <span class="llb-chip">${data.board[i].total_.turretKills}</span>
+                      </div>
+                          
+                      <!-- First Blood -->
+                      <div class="llb-cell llb-right">
+                        <span class="llb-chip">${data.board[i].firstBloodKill}</span>
+                        </div>
+                          
+                      <div class="llb-cell llb-right">
+                        <span class="llb-chip">${data.board[i].objectives}</span>
+                      </div>
+                          
+                      <!-- Objectives Stolen -->
+                      <div class="llb-cell llb-right">
+                        <span class="llb-chip">${data.board[i].total_.objectivesStolen}</span>
+                      </div>
+                    </div>
                 `;
             }
         } else {
