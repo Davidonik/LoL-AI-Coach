5f93dbce9c1d3fab70e91943d2efea7f9aab5896
David Pham <davidphamlong0@gmail.com>
2025-11-10 14:09:58 -0600

    linux server

--- server/launchserver.sh
+++ server/launchserver.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 # Exit immediately if a command fails
 set -e
 
@@ -7,7 +8,7 @@ cd "$(dirname "$0")"
 
 # Set environment variables
 export FLASK_APP=s_api_request.py
-export FLASK_ENV=development
+export FLASK_ENV=production  # Use production mode on Render
 
-# Start the Flask server
-flask run --host=0.0.0.0 --port=10000
+# Start the Flask server on the port Render provides
+flask run --host=0.0.0.0 --port=$PORT
