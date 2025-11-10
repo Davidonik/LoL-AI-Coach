9bf6cfcc25ef6997fdb92752b975c0f6695eec5f
Figo Fontana <Figo>
2025-11-10 13:26:13 -0600

    lb

--- static/s_load_leaderboard.js
+++ static/s_load_leaderboard.js
@@ -18,48 +18,37 @@ document.addEventListener("DOMContentLoaded", async () => {
                 leaderboard_display.innerHTML += `
                     <div class="llb-row">
                       <!-- Summoner -->
-                      <div class="llb-cell">
-                        <div class="llb-sum">
-                          <div class="llb-name">
-                            ${data.board[i].ign_.gameName} <span class="llb-tag">${data.board[i].ign_.tagLine}</span>
-                          </div>
+                      <div class="llb-sum">
+                        <img class="llb-ava" src="/static/media/HelmetBro.png" alt="">
+                        <div class="llb-name">
+                          ${data.board[i].ign_.gameName}
+                          <span class="llb-tag">${data.board[i].ign_.tagLine}</span>
                         </div>
                       </div>
-                          
-                      <!-- KDA -->
-                      <div class="llb-cell">
-                        <span class="llb-kda">${(data.board[i].KDA_.kills + data.board[i].KDA_.assists)/Math.max(data.board[i].KDA_.deaths, 1)}</span>
-                        <span class="llb-kda-detail">${data.board[i].KDA_.kills} / ${data.board[i].KDA_.deaths} / ${data.board[i].KDA_.assists}</span>
+
+                      <!-- K/D/A + KDA -->
+                      <div class="llb-kda-wrap">
+                        <div class="llb-kda-line"><span class="llb-k">${data.board[i].KDA_.kills}</span>/<span class="llb-d">${data.board[i].KDA_.deaths}</span>/<span class="llb-a">${data.board[i].KDA_.assists}</span></div>
+                        <div class="llb-kda">KDA ${(data.board[i].KDA_.kills + data.board[i].KDA_.assists)/Math.max(data.board[i].KDA_.deaths, 1)}</div>
                       </div>
-                          
+
                       <!-- Damage -->
-                      <div class="llb-cell llb-right">
-                        <span class="llb-chip">${data.board[i].total_.totalDamageDealtToChampions}</span>
-                      </div>
-                          
+                      <div><span class="llb-chip">${data.board[i].total_.totalDamageDealtToChampions}</span></div>
+
                       <!-- Gold -->
-                      <div class="llb-cell llb-right">
-                        <span class="llb-chip">${data.board[i].total_.goldEarned}</span>
-                      </div>
-                          
+                      <div><span class="llb-chip">${data.board[i].total_.goldEarned}</span></div>
+
                       <!-- Turrets -->
-                      <div class="llb-cell llb-right">
-                        <span class="llb-chip">${data.board[i].total_.turretKills}</span>
-                      </div>
-                          
-                      <!-- First Blood -->
-                      <div class="llb-cell llb-right">
-                        <span class="llb-chip">${data.board[i].firstBloodKill}</span>
-                        </div>
-                          
-                      <div class="llb-cell llb-right">
-                        <span class="llb-chip">${data.board[i].objectives}</span>
-                      </div>
-                          
-                      <!-- Objectives Stolen -->
-                      <div class="llb-cell llb-right">
-                        <span class="llb-chip">${data.board[i].total_.objectivesStolen}</span>
-                      </div>
+                      <div><span class="llb-chip">${data.board[i].total_.turretKills}</span></div>
+
+                      <!-- First blood -->
+                      <div><span class="llb-chip">${data.board[i].firstBloodKill}</span></div>
+
+                      <!-- Objectives -->
+                      <div><span class="llb-chip">${data.board[i].objectives}</span></div>
+
+                      <!-- Objectives stolen -->
+                      <div><span class="llb-chip">${data.board[i].total_.objectivesStolen}</span></div>
                     </div>
                 `;
             }
